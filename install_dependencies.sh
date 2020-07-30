969c10ce81844077ba96b3f28ea11f888a756549
bruno miranda <bruno.silva>
2020-07-30 20:25:03 -0300

    feat(REACT): [BR] simple application with React js

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Install ESlint :)"
+sudo npm i eslint
+
+echo "Install Prettier :)"
+sudo npm install --save-dev --save-exact prettier
+
+echo "Install Husky - Git hooks made easy :)"
+sudo npm install husky --save-dev
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => <h1 id='main-header'>The Best Stacks</h1>;
+
+export default Header;
